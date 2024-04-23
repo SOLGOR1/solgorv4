aae01bce3f92c6a578d71e2fcd99e3ba51c23dfb
SOLGOR1 <info.sorgol@proton.me>
2024-04-24 00:03:26 +0200

    Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 const DmSans = DM_Sans({
   subsets: ['latin'],
@@ -23,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={DmSans.variable}>
       <body className="bg-[#111111] text-white">
+        <Analytics/>
         <Header />
         <main>{children}</main>
         <Footer />
